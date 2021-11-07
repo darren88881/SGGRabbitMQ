972850bf8d4073f34ac5d9a6f1565e4140325f2c
darren88881 <darren88881@outlook.com>
2021-11-07 13:56:59 +0800

    logback集成

--- src/test/java/com/itgu/AppTest.java
+++ src/test/java/com/itgu/AppTest.java
@@ -1,20 +1,27 @@
 package com.itgu;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
+
+    private Logger logger = LoggerFactory.getLogger(AppTest.class);
+
     /**
-     * Rigorous Test :-)
+     * 日志打印测试
+     * TRACE 不会打印是因为这个日志级别高于 logback.xml 中定义的 DEBUG
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void logOutputTest(){
+        logger.error("ERROR");
+        logger.warn("WARN");
+        logger.info("INFO");
+        logger.debug("DEBUG");
+        logger.trace("TRACE");
     }
 }
